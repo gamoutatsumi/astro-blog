877b60999f44196d7b345dc2cb15d246a3bb0e68
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-01-31 09:46:10 +0900

    add rss feed

---
+++ src/pages/posts/[category]/rss.xml.ts
@@ -0,0 +1,35 @@
+import type { APIContext, GetStaticPaths } from "astro";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const blogEntries = await getCollection("posts");
+  return blogEntries
+    .filter((entry) => !entry.data.isDraft)
+    .map((entry) => {
+      const category = entry.slug.split("/")[0];
+      return {
+        params: { category },
+        props: { category },
+      };
+    });
+};
+
+export const get = async (context: APIContext) => {
+  const entries = await getCollection("posts");
+  const items = entries
+    .filter((entry) => entry.slug.split("/")[0] === context.params.category)
+    .map((entry) => ({
+      title: entry.data.title,
+      link: entry.slug,
+      pubDate: entry.data.publishDate,
+    }));
+
+  return rss({
+    title: "通算n度目のブログ",
+    description: "@gamoutatsumi のブログ",
+    site: context.url.toString(),
+    items,
+    stylesheet: "/rss.xsl",
+  });
+};
