d6a3c72529fc8e7d40a1c36725cadde8b9a46b9b
gamoutatsumi <tatsumi@gamou.dev>
2023-09-28 16:23:03 +0900

    use squoosh

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -15,6 +15,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.gamou.dev",
+  image: { service: squooshImageService() },
   integrations: [
     react(),
     sitemap({
