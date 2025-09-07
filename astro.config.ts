5d8dbc0752ec29f922478da562a075ff06f0f973
gamoutatsumi <tatsumi@gamou.dev>
2025-09-08 00:27:15 +0900

    fix biome settings

--- astro.config.ts
+++ astro.config.ts
@@ -1,31 +1,29 @@
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import partytown from "@astrojs/partytown";
-
-import UnoCSS from "unocss/astro";
-
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 import mcp from "astro-mcp";
+import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.gamou.dev",
-  integrations: [
-    UnoCSS({ injectReset: true }),
-    sitemap({
-      customPages: ["https://blog.gamou.dev"],
-      serialize(item) {
-        if (/\/nsfw/.test(item.url)) {
-          return undefined;
-        }
-        return item;
-      },
-    }),
-    partytown({
-      config: {},
-    }),
-    mcp(),
-  ],
+	site: "https://blog.gamou.dev",
+	integrations: [
+		UnoCSS({ injectReset: true }),
+		sitemap({
+			customPages: ["https://blog.gamou.dev"],
+			serialize(item) {
+				if (/\/nsfw/.test(item.url)) {
+					return undefined;
+				}
+				return item;
+			},
+		}),
+		partytown({
+			config: {},
+		}),
+		mcp(),
+	],
 });
