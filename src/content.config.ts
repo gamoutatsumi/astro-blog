4b0589834e37436b8916d10f6ae053fe6346454f
gamoutatsumi <tatsumi@gamou.dev>
2026-01-26 01:10:23 +0900

    update

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,7 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
-    image: z.string().optional().default("/no_image_yoko.jpg"),
+    image: z.string().default("/no_image_yoko.jpg"),
     publishDate: z.coerce.date(),
     isDraft: z.boolean(),
   }),
